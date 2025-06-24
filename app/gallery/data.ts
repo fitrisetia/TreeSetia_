bc68615adf54b299e9042ede3b8df737215f948c
TreeSetia <fitrisetia2003@gmail.com>
2025-06-24 22:52:33 +0700

    Update data.ts

--- app/gallery/data.ts
+++ app/gallery/data.ts
@@ -20,7 +20,7 @@ export const galleryData: GalleryItem[] = [
   { id: 3, title: 'Jalanan Kota Malam Hari', category: 'Photography', src: 'https://i.pinimg.com/736x/47/d1/12/47d112f63551cbb82f6b4cfcd21782d2.jpg', alt: 'Suasana jalanan kota di malam hari', width: 1920, height: 1080 },
   { id: 4, title: 'Arsitektur Website Portofolio', category: 'Dokumentasi', src: 'https://i.pinimg.com/736x/dc/87/ba/dc87ba4c3fedef9c921b67c011e81f9f.jpg', alt: 'Cuplikan kode arsitektur website', width: 1200, height: 800 },
   { id: 5, title: 'Potret Hitam Putih', category: 'Photography', src: 'https://i.pinimg.com/736x/3c/99/b8/3c99b87750692970c0884e2f58bec9d9.jpg', alt: 'Foto potret hitam putih', width: 800, height: 1200 }, // Ini mungkin potret
-  { id: 6, title: 'Perihal Kopi', category: 'Dokumentasi', src: 'blob:https://id.pinterest.com/7c0483f1-dedf-4884-b32c-e5ea206851ca', alt: 'Perihal Kopi', width: 1280, height: 800 },
+  { id: 6, title: 'Perihal Kopi', category: 'Dokumentasi', src: 'https://i.pinimg.com/736x/81/30/63/8130633f4bda1184bc65cc7a08a2318b.jpg', alt: 'Perihal Kopi', width: 1280, height: 800 },
   { id: 7, title: 'Detail Daun', category: 'Photography', src: 'https://i.pinimg.com/736x/6f/6a/4b/6f6a4b509a7fe895b3aac995cf50d3dd.jpg', alt: 'Foto makro detail daun', width: 1000, height: 750 },
   { id: 8, title: 'Gedung Pencakar Langit', category: 'Photography', src: 'https://i.pinimg.com/736x/87/b0/b1/87b0b1ba5d807e8be9ecbcacc732fd52.jpg', alt: 'Foto arsitektur gedung pencakar langit', width: 1600, height: 1000 },
 ];
